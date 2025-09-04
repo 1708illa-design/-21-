f529db7c1fcee58e9d5e5617d758dee6ff81295d
1708illa-design <1708illa@gmail.com>
2025-09-04 13:59:36 +0300

    Add files via upload

---
+++ Романюк Ілля 2.0 Лаб 1 .py
@@ -0,0 +1,41 @@
+# 1 (вік)
+age = 6
+print("age",type(age),":",age)#цілі числа
+#2 (вага)
+weight = 2.1
+print("weight",type (weight),":", weight)#числа з десятками та тд
+#3 (ім'я)
+name = "Ілля"
+print("name",type(name),":", name)#символи букви
+#4 (тру фолс )
+is_student = True
+print("is_student",type(is_student),":", is_student)#так/ні
+'''#5пустий список ,назви не найшов (можна самому в писати )
+symbol = (input('[     ] '))
+print(f" {symbol}")
+print("symbol",type(symbol),":", symbol)
+'''
+#6 12собак
+name_team = [12,"dog"]
+print("name_team",type(name_team),":", name_team)
+#7список продуктів
+list_food = {1.2, "apple"}
+print("list_food",type(list_food),":", list_food)
+
+#8 (список )
+prize_places = 1,2,3
+print("prize_places",type(prize_places), ":", prize_places)#кортеж
+#9 список групи
+taem_21KI = {1,"Ілля",1,"Денис ",1,"Антон"}
+print("team_21KI",type(taem_21KI),":", taem_21KI)
+#10Види обчислень
+
+a=5
+b=10
+print('a + b=',a+b) #додавання
+print('a - b=',a-b) #віднімання
+print('a * b=',a*b) #множення
+print('a / b=',a/b) #ділення
+print('a % b=',a%b) #остача
+print('a**b=',a**b) #степінь
+print('a // b=',a//b) #ділення без остачі
