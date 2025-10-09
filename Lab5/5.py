0a6ac6b3315507692f22785082b16169bcc83c04
1708illa-design <1708illa@gmail.com>
2025-10-09 13:30:38 +0300

    5.py

--- Lab5/5.py
+++ Lab5/5.py
@@ -2,47 +2,44 @@
 import requests
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import datetime
 import pyttsx3
 
-# 1️⃣ Requests – отримання даних з вебу
+#1️ Requests – отримання даних з вебу
 try:
     response = requests.get("https://api.github.com")
-    print("Requests ✅: Статус відповіді =", response.status_code)
+    print("Requests : Статус відповіді =", response.status_code)
 except Exception as e:
     print("Помилка у requests:", e)
 
-# 2️⃣ NumPy – обчислення середнього значення
+#2️ NumPy – обчислення середнього значення
 try:
     arr = np.array([5, 10, 15, 20])
-    print("NumPy ✅: Середнє значення =", np.mean(arr))
+    print("NumPy : Середнє значення =", np.mean(arr))
 except Exception as e:
     print("Помилка у NumPy:", e)
 
-# 3️⃣ Pandas – створення таблиці (DataFrame)
+#3️ Pandas – створення таблиці (DataFrame)
 try:
     data = {'Імʼя': ['Ілля', 'Анна', 'Роман'], 'Оцінка': [90, 85, 95]}
     df = pd.DataFrame(data)
-    print("Pandas ✅:\n", df)
+    print("Pandas :\n", df)
 except Exception as e:
     print("Помилка у Pandas:", e)
 
-# 4️⃣ Matplotlib – побудова простого графіка
+#4️ Datetime - поточний час
 try:
-    plt.plot([1, 2, 3], [2, 4, 1])
-    plt.title("Графік приклад")
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.savefig("grafik.png")
-    print("Matplotlib ✅: графік збережено у файл grafik.png")
+    now = datetime.datetime.now()
+    formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    print("Datetime ✅: Поточна дата та час =", formatted_time)
 except Exception as e:
-    print("Помилка у Matplotlib:", e)
+    print("Помилка у datetime:", e)
 
-# 5️⃣ pyttsx3 – озвучення тексту
+#5️ pyttsx3 – озвучення тексту
 try:
     engine = pyttsx3.init()
-    engine.say("Hello world is pyttsx3.")
+    engine.say("Hello Taras Anatoliyovich.")
     engine.runAndWait()
-    print("pyttsx3 ✅: текст озвучено")
+    print("pyttsx3 : текст озвучено")
 except Exception as e:
     print("Помилка у pyttsx3:", e)
