74a97d679934bdee9ca3920cccdfc85c105f1e0e
1708illa-design <1708illa@gmail.com>
2025-11-20 16:43:56 +0200

    Add files via upload

---
+++ Lab7/lab 7.py
@@ -0,0 +1,16 @@
+import telebot
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+bot = telebot.TeleBot(os.getenv("TOKEN"))
+
+
+@bot.message_handler(func=lambda a: True) #func=lambda завжди повертає True(пропускає всі смс)
+def echo(a):
+    bot.reply_to(a, a.text) #a(обєкт там де id,час), a.text(сам текст)
+
+def main():
+    bot.infinity_polling() #безкінечно перевіряє, чи прийшло нове повідомлення
+if __name__ == "__main__":
+    main()
