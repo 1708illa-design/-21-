51ff33e3a226e072bd53726cdd0b56198c111bdd
1708illa-design <1708illa@gmail.com>
2025-11-25 22:05:29 +0200

    Update loader.py

--- CARGO/loader.py
+++ CARGO/loader.py
@@ -2,15 +2,6 @@ import os
 from telebot import TeleBot
 from dotenv import load_dotenv
 
-# === ХАК ДЛЯ ВИПРАВЛЕННЯ ПОМИЛКИ ПІДКЛЮЧЕННЯ ===
-# Це змушує Python ігнорувати системні проксі Windows,
-# які часто викликають TimeoutError
-os.environ['HTTP_PROXY'] = ''
-os.environ['HTTPS_PROXY'] = ''
-os.environ['http_proxy'] = ''
-os.environ['https_proxy'] = ''
-os.environ['NO_PROXY'] = '*'
-# ===============================================
 
 # Завантажуємо змінні оточення
 load_dotenv()
@@ -19,4 +10,5 @@ load_dotenv()
 token = os.getenv("TOKEN")
 
 # Створюємо бота
+
 bot = TeleBot(token)
